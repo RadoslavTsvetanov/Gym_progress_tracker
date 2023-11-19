a0ed3dd374edd773cb2cb606e43f7d99398e7cb8
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-11-19 13:05:02 +0200

    after a bit of research I improved the docker starter using multhithreading

--- RUN_DOCKE.py
+++ RUN_DOCKE.py
@@ -1,13 +1,16 @@
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 
-def docker_build_producer(name, building, run, directory):
-    return {
-        'directory': directory,
-        'name': name,
-        'build': f"{building} {name} .",
-        'run': f"{run} {name}"
-    }
+class CommandFactory:
+    @staticmethod
+    def create_command(name, building, run, directory):
+        return {
+            'directory': directory,
+            'name': name,
+            'build': f"{building} {name} .",
+            'run': f"{run} {name}"
+        }
 
 
 def build_container(command_to_run, directory_path):
@@ -26,10 +29,29 @@ def run_container(command_to_build, command_to_run, directory):
         print(f"Command '{command_to_run}' failed with error: {e}")
 
 
+def execute_commands(commands):
+    with ThreadPoolExecutor(max_workers=len(commands)) as executor:
+        futures = {executor.submit(
+            run_container, command['build'], command['run'], command['directory']): command for command in commands}
+        for future in futures:
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Command execution failed: {e}")
+
+
 def main():
-    command = docker_build_producer(
-        "noti2", "docker build -t", "docker run -p 3008:3008", r"C:\Users\Hrisi\Desktop\Gym_progress_tracker\gym\notification_service")
-    run_container(command['build'], command['run'], command['directory'])
+    command_factory = CommandFactory()
+    commands = [
+        command_factory.create_command(
+            "noti2", "docker build -t", "docker run -p 3008:3008", r"C:\Users\Hrisi\Desktop\Gym_progress_tracker\gym\notification_service"),
+        command_factory.create_command(
+            "exercises_service", "docker build -t", "docker run -p 3007:3007", r"C:\Users\Hrisi\Desktop\Gym_progress_tracker\gym\exercises_service")
+        # Add more commands here as needed
+    ]
+
+    execute_commands(commands)
 
 
-main()
+if __name__ == "__main__":
+    main()
