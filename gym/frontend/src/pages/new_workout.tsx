895bdcc0ac06a787c1b7b38289f554f96b3e9437
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-11-30 22:19:03 +0200

    improved the conditional rendering of the main page

--- gym/frontend/src/pages/new_workout.tsx
+++ gym/frontend/src/pages/new_workout.tsx
@@ -1,26 +1,43 @@
 "use client"
+import { User } from "~/utils/types"
 import { CookieHandler } from "~/utils/cookie_handler";
 import { api } from "~/utils/api";
-import {useEffect} from "react"
+import {useEffect,useState} from "react"
 interface FormData{
 
 }
 export default function Workout() {
     const cookie_handler = new CookieHandler;
-    const use
-    const data = api.post.get_exercises.useQuery({username:})
+    const [user,set_user] = useState<User | undefined>(undefined)
+    const data = api.post.get_exercises.useQuery(user != undefined ?{username:user.username,token:user.toekn}:{username:"",token:""})
     useEffect(() => {
-        const cookie: string | boolean = cookie_handler.checkForCookie('user')
-        if (!cookie) {
-            window.location.href = "/login"
-            return //! very important
-        }
+       const cookie: string | boolean = cookie_handler.checkForCookie('user');
+        cookie_handler.redirectToLoginIfNoCookie(cookie);
+
+        if (typeof cookie === 'string') {
+            const user: User | undefined = cookie_handler.parseCookieToUser(cookie);
+            set_user(user);//TODO later to be merged into just two function - the whole functionality in the useEffect
+  }
+  
+
+        function submit_form() {
+      
+  }
     },[])
     return (
         <div>
-            <form>
-                <input/>
-            </form>
+            {user != undefined
+                ?
+                    (<form>
+                        <input/>
+                    </form>)
+                :
+                (
+                    <div>
+                        Loading ...
+                    </div>
+                    )
+            }
         </div>
     )
 }
