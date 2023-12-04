62d1bdf8c674c1f749ffb1ae6cfbb67fd1e4866b
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-12-04 02:04:12 +0200

    implemented formating logic for the input

--- gym/frontend/src/pages/new_workout.tsx
+++ gym/frontend/src/pages/new_workout.tsx
@@ -3,18 +3,32 @@ import { User, Exercise, Workout, Program, ExercisesData } from "~/utils/types";
 import { CookieHandler } from "~/utils/cookie_handler";
 import { api } from "~/utils/api";
 import { useEffect, useState } from "react";
-
+import { Helper_functions } from "~/utils/helper_functions";
 interface FormData {
   // Define your form data interface if needed
 }
-function render_workout_exercises_based_on_type(type: string, workouts: Workout[]) {
+interface Workout_component_props{
+    type: string,
+    exercises:object
+}
+function previous_workout(workout:Workout_component_props){
+    return (
+        <div>
+            {workout.type}
+        </div >
+    )
+}
+
+
+
+function render_workout_exercises_based_on_type(type: string, workouts: Workout[], handle_change: void) {
   const selectedWorkout = workouts.find((workout) => workout.type === type);
 
   function renderExerciseInputs(exercise: Exercise) {
     const setInputs = [];
 
     for (let setIndex = 1; setIndex <= exercise.sets; setIndex++) {
-      const inputId = `exercise-${exercise.id}-set-${setIndex}`;
+      const inputId = `${exercise.name}-${setIndex}`;
 
       setInputs.push(
         <div key={setIndex}>
@@ -24,6 +38,7 @@ function render_workout_exercises_based_on_type(type: string, workouts: Workout[
             id={inputId}
             name={inputId}
             // Add value and onChange if you want to manage input state
+            onChange={handle_change}
           />
         </div>
       );
@@ -42,7 +57,7 @@ function render_workout_exercises_based_on_type(type: string, workouts: Workout[
 
 
 export default function Workout() {
-  const [selectedWorkout, set_selected_workout] = useState<string>("");
+    const [selectedWorkout, set_selected_workout] = useState<string>("");
   const [formContent, setFormContent] = useState<FormData>({});
   const cookie_handler = new CookieHandler();
   const [current_user, set_user] = useState<User | undefined>();
@@ -64,10 +79,25 @@ export default function Workout() {
 
   const programData = exercises.data ? exercises.data.data : {};
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormContent((prevContent) => ({
+      ...prevContent,
+      [name]: value,
+    }));
+  };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        const helper = new Helper_functions; 
+        
+        const format_exercises = helper.format_json(formContent)
     event.preventDefault();
+      let data_to_be_sent = {
+        type: selectedWorkout,
+        exercises: format_exercises
+    }
     // Implement your form submission logic here using formContent state
-    console.log("Form submitted with content:", formContent);
+    console.log("Form submitted with content:", data_to_be_sent);
   };
 
   return (
@@ -78,22 +108,26 @@ export default function Workout() {
             <select
               onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
                 set_selected_workout(event.target.value);
+                //!clear form content so that data from the old input is clear
+                setFormContent({})
               }}
             >
               {programData.exercises.program.workouts.map((workout: Workout) => (
                 <option key={workout.id} value={workout.type}>
-                  {workout.type}
+                  {workout.type} 
                 </option>
               ))}
             </select>
             {render_workout_exercises_based_on_type(
               selectedWorkout,
-              programData.exercises.program.workouts
+                programData.exercises.program.workouts,
+              handleChange 
             )}
             <button type="submit">Submit</button>
           </form>
-          <div>{selectedWorkout}</div>
-        </div>
+                  <div>{selectedWorkout}</div>
+                  {previous_workout({ type: selectedWorkout, exercises:{}})}
+              </div>
       ) : (
         <div>Loading ...</div>
       )}
--- gym/frontend/src/utils/helper_functions.ts
+++ gym/frontend/src/utils/helper_functions.ts
@@ -1,10 +1,27 @@
+class Helper_functions{
+     find_dash_index(string:string) {
+        for (let i = 0; i < string.length; i++) {
+            if(string[i] == "-"){
+                return i;
+            }
+        }
+    }
 
-function find_latest_of_type(arr: any, type: string): any{
-    
-    
+     format_json(data) {
+    const new_data = {};
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            let index_of_dash = this.find_dash_index(key);
+            let new_key = key.slice(0, index_of_dash);
+            // Initialize new_data[new_key] as an array if not already defined
+            new_data[new_key] = new_data[new_key] || [];
+            new_data[new_key].push(data[key]);
+        }
+    }
+    return new_data;
 }
 
-
+}
 export {
-
+    Helper_functions
  }
