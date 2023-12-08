bb93c81fbc36bca4ae73e01cd994b494659a5463
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-12-09 01:51:16 +0200

    new_workout shows previous workout of type

--- gym/frontend/src/components/render_progress_workouts.tsx
+++ gym/frontend/src/components/render_progress_workouts.tsx
@@ -1,32 +1,54 @@
+import React from 'react';
+
+interface ProgressedExercise {
+  name: string;
+  sets: string[];
+  que: string[];
+}
+
+interface ExerciseGroup {
+  type: string;
+  exercises: ProgressedExercise[];
+}
 
 interface PreviousWorkoutComponentProps {
   type: string;
-  exercises: object;
+  exercises: { progression: { workouts: ExerciseGroup[] } };
 }
 
-const PreviousWorkout = (workout: PreviousWorkoutComponentProps) => {
-  const workouts_of_type = ((workout.type != undefined && workout.type != "") ? (workout.exercises.progression[0].workouts.filter(Workout => Workout.type === workout.type)) : (workout.exercises.progression[0].workouts))
+const PreviousWorkout: React.FC<PreviousWorkoutComponentProps> = (props) => {
+  const { type, exercises } = props;
+  const workoutProgression = exercises.progression[0].workouts;
+
+  let lastWorkoutOfType: ExerciseGroup | undefined;
+  if (type && type !== '') {
+    lastWorkoutOfType = workoutProgression.find(
+      (workout) => workout.type === type
+    );
+  }
+
+  const lastWorkout = lastWorkoutOfType || workoutProgression[workoutProgression.length - 1];
+
+  if (!lastWorkout) {
+    return <div>No previous workout found.</div>;
+  }
+
   return (
     <div className="bg-gray-200 p-4 mb-4">
       <h1>__</h1>
-      <h2 className="text-lg font-semibold">Previous {workout.type} workout</h2>
-      {workouts_of_type.map((exerciseGroup, index) => (
-        <div key={index} className="mb-4">
-          <h3>{exerciseGroup.type}</h3>
-          {exerciseGroup.exercises.map((exercise : Progressed_exercise, idx) => (
-            <div key={idx} className="mb-2">
-              <h4>{exercise.name}</h4>
-              <p>Sets: {exercise.sets.join(", ")}</p>
-              <p>Que: {exercise.que.join(", ")}</p>
-            </div>
-          ))}
-        </div>
-      ))}
+      <h2 className="text-lg font-semibold">Previous {type} workout</h2>
+      <div>
+        <h3>{lastWorkout.type}</h3>
+        {lastWorkout.exercises.map((exercise: ProgressedExercise, idx) => (
+          <div key={idx} className="mb-2">
+            <h4>{exercise.name}</h4>
+            <p>Sets: {exercise.sets.join(', ')}</p>
+            <p>Que: {exercise.que.join(', ')}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-
-export {
-PreviousWorkout
-}
+export { PreviousWorkout };
