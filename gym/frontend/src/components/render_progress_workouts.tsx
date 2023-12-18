be5ea6b2691323e5e217fd00f1523376038b0fee
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-12-18 22:35:25 +0200

    finished history

--- gym/frontend/src/components/render_progress_workouts.tsx
+++ gym/frontend/src/components/render_progress_workouts.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface ProgressedExercise {
   name: string;
   sets: string[];
-  que: string[];
+  que?: string[]; // que is optional
 }
 
 interface ExerciseGroup {
@@ -14,42 +14,63 @@ interface ExerciseGroup {
 interface PreviousWorkoutComponentProps {
   type: string;
   exercises: { progression: { workouts: ExerciseGroup[] } };
+  renderAll?: boolean; // renderAll prop is optional
 }
 
 const PreviousWorkout: React.FC<PreviousWorkoutComponentProps> = (props) => {
-  const { type, exercises } = props;
-  const workoutProgression = exercises != undefined ? exercises.progression[0].workouts : undefined;
+  const { type, exercises, renderAll } = props;
+  const workoutProgression = exercises?.progression[0]?.workouts;
 
   let lastWorkoutOfType: ExerciseGroup | undefined;
   if (type && type !== '') {
-    lastWorkoutOfType = workoutProgression.find(
+    lastWorkoutOfType = workoutProgression?.find(
       (workout) => workout.type === type
     );
   }
 
-  const lastWorkout = lastWorkoutOfType || workoutProgression[workoutProgression.length - 1];
+  const lastWorkout = lastWorkoutOfType || workoutProgression?.[workoutProgression.length - 1];
 
   if (!lastWorkout) {
     return <div>No previous workout found.</div>;
   }
 
   return (
-    <div className="bg-gray-200 p-4 mb-4">
-      {workoutProgression != undefined && (
+    <div className="bg-gray-900 text-white rounded-md shadow-md p-4 mb-4">
+      {workoutProgression && (
         <>
-        <h1>__</h1>
-      <h2 className="text-lg font-semibold">Previous {type} workout</h2>
-      <div>
-        <h3>{lastWorkout.type}</h3>
-        {lastWorkout.exercises.map((exercise: ProgressedExercise, idx) => (
-          <div key={idx} className="mb-2">
-            <h4>{exercise.name}</h4>
-            <p>Sets: {exercise.sets.join(', ')}</p>
-            <p>Que: {(exercise.que != undefined) ? (exercise.que.join(', ')) : (<></>)}</p>
+          <h1 className="text-4xl mb-4">Workout Details</h1>
+          <h2 className="text-lg font-semibold">Previous {type} workout</h2>
+          <div>
+            <h3>{lastWorkout.type}</h3>
+            {!renderAll ? (
+              lastWorkout.exercises.map((exercise: ProgressedExercise, idx) => (
+                <div key={idx} className="mb-4">
+                  <h4>{exercise.name}</h4>
+                  <p className="text-red-400">Sets: {exercise.sets.join(', ')}</p>
+                  {exercise.que && (
+                    <p className="text-red-400">Que: {exercise.que.join(', ')}</p>
+                  )}
+                </div>
+              ))
+            ) : (
+              workoutProgression.map((workout: ExerciseGroup, index: number) => (
+                <div key={index}>
+                  <h3>{workout.type}</h3>
+                  {workout.exercises.map((exercise: ProgressedExercise, idx: number) => (
+                    <div key={`${index}-${idx}`} className="mb-4">
+                      <h4>{exercise.name}</h4>
+                      <p className="text-red-400">Sets: {exercise.sets.join(', ')}</p>
+                      {exercise.que && (
+                        <p className="text-red-400">Que: {exercise.que.join(', ')}</p>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              ))
+            )}
           </div>
-        ))}
-          </div>
-      </>)}
+        </>
+      )}
     </div>
   );
 };
--- gym/frontend/src/pages/history.tsx
+++ gym/frontend/src/pages/history.tsx
@@ -9,8 +9,13 @@ import Cookies from "js-cookie"
 import { User,Exercise, Workout, Program } from "~/utils/types"
 import { application_url } from "./constant_variables/constants"
 import { Helper_functions } from "~/utils/helper_functions";
+function Filter() {
+
+}
+
+
 export default function History() {
-    const [type_of_workout,set_type_of_workout] = useState<string | undefined>()
+    const [type_of_workout, set_type_of_workout] = useState<string | undefined>()
     const cookie_helper = new CookieHandler;
     const [user,set_user] = useState<User | undefined>()
     const progress: object | undefined = api.post.get_progression.useQuery((user != undefined ? {username:user.username,token:user.token}:{}),{enabled:user != undefined})
@@ -19,9 +24,9 @@ export default function History() {
     },[])
     return (
         <>
-        {(progress != undefined && progress.data != undefined && user != undefined) ? ( <div>
+        {(progress != undefined && progress.data != undefined && user != undefined && progress.data.data != undefined) ? ( <div>
                 <p>{console.log(progress.data.data.progression[0].workouts)}</p>
-                {PreviousWorkout({type:type_of_workout,exercises:progress.data.data})}
+                {PreviousWorkout({type:type_of_workout,exercises:progress.data.data,renderAll:true})}
             </div>): <div>Loading ...</div>}
         </>
     )
